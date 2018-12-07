e8db0618b514553b573fa9b8c576c01a358e5c7b
高翔 <371329358@qq.com>
2018-12-07 15:22:49 +0800

    eureka服务

--- eureka/src/main/java/com/snail/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/snail/eureka/EurekaApplication.java
@@ -2,8 +2,10 @@ package com.snail.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer  //启动eureka注册中心server
 public class EurekaApplication {
 
     public static void main(String[] args) {
