f8a036b70125e1caa3b5ca25f49a212cbd721dab
高翔 <371329358@qq.com>
2018-12-27 18:08:44 +0800

    微服务注册到Eureka Server

--- user-micro-service/src/main/java/com/snail/user/UserMicroServiceApplication.java
+++ user-micro-service/src/main/java/com/snail/user/UserMicroServiceApplication.java
@@ -3,10 +3,10 @@ package com.snail.user;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 //@EnableFeignClients
 @SpringBootApplication
 @MapperScan("com.snail.user.mapper")
